3af74ec67a69d8a2c9c9ab235ef8109e82bc80e7
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2020-06-29 15:02:14 +0530

    Made the search results clickable

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,19 +1,15 @@
 function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      a[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      a[i].style.display = "none";
     }
   }
 }
